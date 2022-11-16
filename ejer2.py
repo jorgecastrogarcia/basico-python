3517250ce49b67fe888c6fe8d827e28ed3a7457f
jorgecastrogarcia <118173846+jorgecastrogarcia@users.noreply.github.com>
2022-11-16 13:29:36 +0100

    Add files via upload

---
+++ ejer2.py
@@ -0,0 +1,13 @@
+notaindividual=float(input('dime la nota del hito individual '))
+notagrupal=float(input('dime la nota del hito grupal '))
+notaexamen=float(input('dime la nota de tu examen '))
+
+
+cuenta1=notaindividual * 0.3
+cuenta2=notagrupal * 0.2
+cuenta3=notaexamen * 0.5
+
+
+
+notatotal= cuenta1 + cuenta2 + cuenta3
+print(notatotal)
